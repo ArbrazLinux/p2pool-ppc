4c434b54353d1341371026f3dda21f7e37c22708
ArbrazLinux <37558498+ArbrazLinux@users.noreply.github.com>
2018-12-03 13:24:13 +0100

    Add files via upload

---
+++ p2pool/bitcoin/networks/peercoin.py
@@ -0,0 +1,31 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'e5e9e8e6'.decode('hex')
+P2P_PORT = 9901
+ADDRESS_VERSION = 50
+RPC_PORT = 9902
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            (yield helper.check_block_header(bitcoind, '0000000032fe677166d54963b62a4677d8957e87c508eaa4fd7eb1c880cd27e3')) and # genesis block
+            (yield helper.check_block_header(bitcoind, '000000000000bca54d9ac17881f94193fd6a270c1bb21c3bf0b37f588a40dbd7')) and # genesis block
+            (yield helper.check_block_header(bitcoind, 'd39d1481a7eecba48932ea5913be58ad3894c7ee6d5a8ba8abeb772c66a6696e')) and # genesis block
+            (yield helper.check_block_header(bitcoind, '27fd5e1de16a4270eb8c68dee2754a64da6312c7c3a0e99a7e6776246be1ee3f')) and # 478559 -- Bitcoin Cash fork
+            (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
+        ))
+SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
+POW_FUNC = data.hash256
+BLOCK_PERIOD = 600 # s
+SYMBOL = 'PPC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Peercoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Peercoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.peercoin'), 'peercoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ppc/block.dws'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ppc/address.dws?'
+TX_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/ppc/tx.dws?'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUMB_SCRYPT_DIFF = 1
+DUST_THRESHOLD = 0.001e7
---
+++ p2pool/networks/peercoin.py
@@ -0,0 +1,30 @@
+from p2pool.bitcoin import networks
+
+# CHAIN_LENGTH = number of shares back client keeps
+# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
+# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
+# REAL_CHAIN_LENGTH must be changed in sync with all other clients
+# changes can be done by changing one, then the other
+
+PARENT = networks.nets['peercoin']
+SHARE_PERIOD = 30 # seconds
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 200 # shares
+SPREAD = 3 # blocks
+IDENTIFIER = '47C2163CDDE6FBC2'.decode('hex')
+PREFIX = '‭A9EEADD30484E297'.decode('hex')
+P2P_PORT = 9993
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**32 - 1
+PERSIST = False
+WORKER_PORT = 9992
+BOOTSTRAP_ADDRS = 'miner-control.de:3330 ppcoin.securepayment.cc:3357'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-ppc'
+VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
+VERSION_WARNING = lambda v: None
+SOFTFORKS_REQUIRED = set(['bip65', 'csv'])
+MINIMUM_PROTOCOL_VERSION = 3301
+NEW_MINIMUM_PROTOCOL_VERSION = 3301
+BLOCK_MAX_SIZE = 1000000
+BLOCK_MAX_WEIGHT = 4000000
