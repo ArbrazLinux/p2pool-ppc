163c35e7010a254f7e8480967a69f7a4cd426306
ArbrazLinux <37558498+ArbrazLinux@users.noreply.github.com>
2018-12-03 15:15:38 +0100

    Update peercoin.py

--- p2pool/bitcoin/networks/peercoin.py
+++ p2pool/bitcoin/networks/peercoin.py
@@ -11,14 +11,9 @@ P2P_PREFIX = 'e5e9e8e6'.decode('hex')
 P2P_PORT = 9901
 ADDRESS_VERSION = 50
 RPC_PORT = 9902
-# RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-#	'peercoin' in (yield bitcoind.rpc_help()) and
-#            (yield helper.check_block_header(bitcoind, '0000000032fe677166d54963b62a4677d8957e87c508eaa4fd7eb1c880cd27e3')) and # genesis block
-#            (yield helper.check_block_header(bitcoind, '000000000000bca54d9ac17881f94193fd6a270c1bb21c3bf0b37f588a40dbd7')) and # chk block
-#            (yield helper.check_block_header(bitcoind, 'd39d1481a7eecba48932ea5913be58ad3894c7ee6d5a8ba8abeb772c66a6696e')) and # chk block
-#            (yield helper.check_block_header(bitcoind, '27fd5e1de16a4270eb8c68dee2754a64da6312c7c3a0e99a7e6776246be1ee3f')) and # 99999 block
-#            (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
-#        ))
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'bitcoin' in (yield bitcoind.rpc_help())
+        ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
